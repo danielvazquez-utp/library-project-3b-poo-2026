35bbb8716637d39d3049a19d112cf732ff4c0a16
M.I. Paulo Daniel Vázquez Mora <73305449+danielvazquez-utp@users.noreply.github.com>
2026-06-10 17:57:21 +0000

    Integración de las librerías admin-lte y bootstrap

--- library-project/src/main.jsx
+++ library-project/src/main.jsx
@@ -1,6 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+// Librerías de AdminLTE
+import 'admin-lte/dist/css/adminlte.min.css'
+import 'admin-lte/dist/js/adminlte.min.js'
+// Librerías de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
